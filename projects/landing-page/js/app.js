947b9579ced84e9f933aca961c171f7c62ddd8b3
Hossam Tarek <hossamtarek250@gmail.com>
2020-08-23 22:20:51 +0200

    Highlight the section that is in the viewport

--- projects/landing-page/js/app.js
+++ projects/landing-page/js/app.js
@@ -26,13 +26,30 @@ const fragment = document.createDocumentFragment();
  * Start Helper Functions
  * 
 */
+function isInViewport(element) {
+    const rect = element.getBoundingClientRect();
+    return (
+        rect.top >= 0 && rect.top < window.innerHeight && rect.left >= 0
+    );
+}
+
+
 function addToNav(section) {
     const listItem = document.createElement("li");
+
+    document.addEventListener("scroll", function() {
+        const sectionName = section.getAttribute("data-nav");
+        if (isInViewport(section)) {
+            section.classList.add("highlighted");
+        } else {
+            section.classList.remove("highlighted");
+        }
+    });
 
     listItem.textContent = section.getAttribute("data-nav");
     listItem.className = "menu__link";
     listItem.addEventListener("click", function() {
-        section.scrollIntoView({behavior: "smooth", block: "start"});
+        section.scrollIntoView({behavior: "smooth", block: "end"});
     });
 
     fragment.appendChild(listItem);
